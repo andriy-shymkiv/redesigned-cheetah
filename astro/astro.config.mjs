d3cf702bea1a42687554ba140d64dfffecbf70da
andriy-shymkiv <winamondezik@gmail.com>
2026-04-23 17:37:25 +0300

    astro + starlight and twap example

---
+++ astro/astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	base: '/docs',
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'API',
+					autogenerate: { directory: 'api' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
