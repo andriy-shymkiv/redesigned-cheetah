aa21d3ead41d28c58b573b3e53211e059eba4d3e
andriy-shymkiv <winamondezik@gmail.com>
2026-05-06 10:55:59 +0300

    astro-keystatic

---
+++ astro-keystatic/.astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/posts/twap-release.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftwap-release.mdoc&astroContentModuleFlag=true")],
+["src/content/posts/other.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fother.mdoc&astroContentModuleFlag=true")]]);
+		
